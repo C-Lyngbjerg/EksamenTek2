d815d273ae757d74bfccb0b9fab79fe91c368995
C-Lyngbjerg <56966598+C-Lyngbjerg@users.noreply.github.com>
2021-01-23 11:48:52 +0100

    Initial commit

---
+++ src/main/java/com/eksamen/tek2/Tek2Application.java
@@ -0,0 +1,13 @@
+package com.eksamen.tek2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tek2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tek2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/eksamen/tek2/Tek2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eksamen.tek2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tek2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
